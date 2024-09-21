41e40e36b68673db28084c1a5987f11d1293741c
dirtylesc <phanxuansy219@gmail.com>
2024-09-21 17:16:59 +0700

    dirtylesc: Define page paths

---
+++ src/constants/paths.js
@@ -0,0 +1,15 @@
+const paths = {
+    HOME: '/',
+
+    //AUTH
+    LOGIN: '/login',
+    REGISTER: '/register',
+    LOGOUT: '/logout',
+
+    //APPS
+    PROJECT_LIST: '/project',
+
+    KANBAN: '/kanban'
+}
+
+export default paths
