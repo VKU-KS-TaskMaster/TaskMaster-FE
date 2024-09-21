e65246869971823062038812d61cf7173535b217
dirtylesc <phanxuansy219@gmail.com>
2024-09-21 17:14:45 +0700

    dirtylesc: Config import path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    css: {
+        devSourcemap: true
+    },
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, './src/components'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@layouts': path.resolve(__dirname, './src/layouts'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@features': path.resolve(__dirname, './src/features'),
+            '@configs': path.resolve(__dirname, './src/configs'),
+            '@constants': path.resolve(__dirname, './src/constants')
+        }
+    }
 })
