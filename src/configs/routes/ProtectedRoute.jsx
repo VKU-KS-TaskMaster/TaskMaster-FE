65807d7832e8ad45e47f2b8f91c4104a067beb05
dirtylesc <phanxuansy219@gmail.com>
2024-09-21 17:15:49 +0700

    dirtylesc: Config router

---
+++ src/configs/routes/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { AppContext } from '@/contexts/app.context'
+import { useContext } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+export default function ProtectedRoute() {
+    const { isAuthenticated } = useContext(AppContext)
+    return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
---
+++ src/configs/routes/appRoute.jsx
@@ -0,0 +1,67 @@
+import paths from '@constants/paths'
+import AuthLayout from '@layouts/AuthLayout'
+import MainLayout from '@layouts/MainLayout'
+import Login from '@pages/Auth/Login'
+// import RejectedRoute from './RejectedRoute'
+// import ProtectedRoute from './ProtectedRoute'
+
+const appRoute = [
+    {
+        path: '',
+        // element: <ProtectedRoute />,
+        children: [
+            {
+                element: <MainLayout />
+                // loader: rootLoader,
+                // children: [
+                //     {
+                //         path: '/',
+                //         element: <Dashboard />
+                //         // loader: rootLoader,
+                //         // children: [],
+                //     },
+                //     {
+                //         path: 'kanban',
+                //         element: <KanbanBoard />
+                //         // loader: teamLoader,
+                //         // children: [],
+                //     },
+                //     {
+                //         path: 'project',
+                //         children: [
+                //             {
+                //                 path: '',
+                //                 element: <ProjectList />
+                //                 // loader: teamLoader,
+                //                 // children: [],
+                //             },
+                //             {
+                //                 path: 'create',
+                //                 element: <ProjectCreate />
+                //                 // loader: teamLoader,
+                //                 // children: [],
+                //             }
+                //         ]
+                //     }
+                // ]
+            }
+        ]
+    },
+    {
+        path: '',
+        // element: <RejectedRoute />,
+        children: [
+            {
+                element: <AuthLayout />,
+                children: [
+                    {
+                        path: paths.LOGIN,
+                        element: <Login />
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+export default appRoute
