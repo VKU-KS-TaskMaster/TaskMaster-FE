3f3ee34a6454ef8867677c573c341f45fee147b8
dirtylesc <phanxuansy219@gmail.com>
2024-09-21 22:33:47 +0700

    dirtylesc: Update page paths

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,5 +1,10 @@
 const paths = {
-  HOME: '/',
+  HOME: '/:spaceCode/',
+
+  USER: '/:spaceCode/user',
+  TEAM: '/:spaceCode/team',
+
+  CHAT: '/:spaceCode/chat',
 
   //AUTH
   LOGIN: '/login',
@@ -7,9 +12,15 @@ const paths = {
   LOGOUT: '/logout',
 
   //APPS
-  PROJECT_LIST: '/project',
+  PROJECT_LIST: '/project_list',
 
-  KANBAN: '/kanban'
+  PROJECT_BOARD: '/project/:code/board',
+  PROJECT_CALENDAR: '/project/:code/calendar',
+  PROJECT_GANTT: '/project/:code/gantt',
+  PROJECT_FILE: '/project/:code/file',
+  PROJECT_DASHBOARD: '/project/:code/dashboard',
+  PROJECT_OVERVIEW: '/project/:code/overview',
+  PROJECT_WORKFLOW: '/project/:code/workflow'
 }
 
 export default paths
