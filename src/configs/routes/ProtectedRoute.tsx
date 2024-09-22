d94bd6f4aad4974332d71322ab17465810c1352a
dirtylesc <phanxuansy219@gmail.com>
2024-09-22 11:41:25 +0700

    dirtylesc: Update Route Wrapper with Redux

--- src/configs/routes/ProtectedRoute.tsx
+++ src/configs/routes/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
-import { AppContext } from '@/contexts/app.context'
-import { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
+import { useAppSelector } from '@features/hook'
+
 export default function ProtectedRoute() {
-  const { isAuthenticated } = useContext(AppContext)
-  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+  const user = useAppSelector((state) => state.auth.user)
+  return !user ? <Outlet /> : <Navigate to='/login' />
 }
