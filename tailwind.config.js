206fc428dbe32f25973f6d36e805070c8909b260
dirtylesc <phanxuansy219@gmail.com>
2024-09-24 17:23:30 +0700

    dirtylesc: Setup tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  corePlugins: {
-    preflight: false
-  },
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        navigation: {
+          text: 'rgba(254, 254, 255, 0.8)',
+          headerBg: 'rgba(46, 46, 48, 1)',
+          sidebarBg: 'rgba(46, 46, 48, 1)',
+          contentBg: 'rgba(37, 38, 40, 1)'
+        },
+
+        btn: {
+          bgHover: 'rgba(255, 255, 255, 0.1)',
+          bgActive: 'rgba(255, 255, 255, 0.2)',
+          border: 'rgba(106, 105, 106, 0.5)',
+          borderHover: 'rgba(106, 105, 106, 1)'
+        },
+
+        bg: {
+          inputModal: 'rgba(37, 38, 40, 1)',
+          success: 'rgba(29, 55, 51, 1)',
+          mess: 'rgba(173, 104, 0, 1)',
+          weak: 'rgba(30, 31, 33, 1)',
+          active: 'rgba(255, 255, 255, 0.11)',
+          card: 'rgba(42, 43, 45, 1)'
+        },
+
+        icon: 'rgba(162, 160, 162, 0.85)',
+        iconActive: 'rgba(255, 255, 255, 1)',
+        borderBottom: 'rgba(66, 66, 68, 1)',
+        text: 'rgba(245, 244, 243, 1)',
+        textWeak: 'rgba(162, 160, 162, 1)',
+        iconSuccess: 'rgba(93, 162, 131, 1)',
+
+        projectItemHover: 'rgba(43, 44, 46, 0.5)'
+      }
+    }
   },
   plugins: []
 }
