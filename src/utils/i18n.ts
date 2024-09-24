168093fad5146a1577ac2caca800626c747be55e
dirtylesc <phanxuansy219@gmail.com>
2024-09-24 17:24:50 +0700

    dirtylesc: Fix path resources

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -44,7 +44,10 @@ i18n
       escapeValue: false
     },
     backend: {
-      backends: [HttpBackend, resourcesToBackend((lng: string, ns: string) => import(`/locales/${lng}/${ns}.json`))],
+      backends: [
+        HttpBackend,
+        resourcesToBackend((lng: string, ns: string) => import(`@constants/translations/${lng}/${ns}.json`))
+      ],
       backendOptions: [
         {
           loadPath: '/locales/{{lng}}/{{ns}}.json'
