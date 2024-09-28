855dd76d4fc765dfe16d27e0b006123cc4d3dd96
dirtylesc <phanxuansy219@gmail.com>
2024-09-28 18:39:18 +0700

    dirtylesc: Config theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
 import { I18nextProvider } from 'react-i18next'
 import { QueryClientProvider } from '@tanstack/react-query'
@@ -7,18 +7,31 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import i18n from '@utils/i18n'
 import appRoute from '@configs/routes/appRoute'
 import queryClient from '@core/queryClient'
+import localStorageKeys, { themeValues } from '@constants/localStorageKeys'
 import { store } from '@features/store'
-import './index.css'
+import { ConfigProvider } from 'antd'
+import { lightTheme, darkTheme } from './theme'
 
 const router = createBrowserRouter(appRoute)
 
 function App() {
+  const [theme, setTheme] = useState(darkTheme)
+
+  useEffect(() => {
+    const language = localStorage.getItem(localStorageKeys.THEME) || themeValues.DARK
+
+    if (language === themeValues.DARK) setTheme(darkTheme)
+    else if (language === themeValues.LIGHT) setTheme(lightTheme)
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <Suspense>
         <Provider store={store}>
           <I18nextProvider i18n={i18n}>
-            <RouterProvider router={router} />
+            <ConfigProvider theme={{ cssVar: true, ...theme }}>
+              <RouterProvider router={router} />
+            </ConfigProvider>
           </I18nextProvider>
         </Provider>
       </Suspense>
