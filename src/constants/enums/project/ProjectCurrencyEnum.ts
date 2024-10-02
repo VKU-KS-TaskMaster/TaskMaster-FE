bde68e4d23d41e4b6519fcaedd9b8c2d79f77b49
dirtylesc <phanxuansy219@gmail.com>
2024-10-02 13:38:04 +0700

    dirtylesc: Define project enums

---
+++ src/constants/enums/project/ProjectCurrencyEnum.ts
@@ -0,0 +1,19 @@
+const PROJECT_CURRENCY_VI = '0'
+const PROJECT_CURRENCY_EN = '1'
+const PROJECT_CURRENCY_KR = '2'
+
+const ProjectCurrencyEnum = {
+  [PROJECT_CURRENCY_VI]: PROJECT_CURRENCY_VI,
+  [PROJECT_CURRENCY_EN]: PROJECT_CURRENCY_EN,
+  [PROJECT_CURRENCY_KR]: PROJECT_CURRENCY_KR
+}
+
+const ProjectCurrencyEnumTransl = {
+  [PROJECT_CURRENCY_VI]: 'constant.currency.vi',
+  [PROJECT_CURRENCY_EN]: 'constant.currency.en',
+  [PROJECT_CURRENCY_KR]: 'constant.currency.kr'
+}
+
+export default ProjectCurrencyEnumTransl
+
+export { PROJECT_CURRENCY_VI, PROJECT_CURRENCY_EN, PROJECT_CURRENCY_KR }
