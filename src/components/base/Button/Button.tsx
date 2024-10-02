608c977dffe392d04561f5f92da2ddbdffd58aec
dirtylesc <phanxuansy219@gmail.com>
2024-10-02 16:32:53 +0700

    dirtylesc: Update ButtonComp with themeMode

--- src/components/base/Button/Button.tsx
+++ src/components/base/Button/Button.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@hooks/useTheme'
 import { cn } from '@utils/base'
 import type { ButtonProps } from 'antd'
 import { Button as BaseButton } from 'antd'
@@ -22,13 +23,24 @@ type ButtonCombination = `${ButtonType}_${ButtonVariant}`
 
 type DefineButtonColorType = Partial<Record<ButtonCombination, string>>
 
-export const DEFINE_BUTTON_CLASS_NAME: DefineButtonColorType = {
+export const DEFINE_BUTTON_CLASS_NAME_DARK: DefineButtonColorType = {
   [`${ButtonType.DEFAULT}_${ButtonVariant.GHOST}`]:
-    'group border-btn-border text-textWeak hover:bg-btn-bgHover hover:text-text active:bg-btn-bgActive active:border-btn-borderHover',
+    'group border-btn-border text-text-weak hover:bg-btn-bgHover hover:text-text-light active:bg-btn-bgActive active:border-btn-borderHover',
   [`${ButtonType.PRIMARY}_${ButtonVariant.GHOST}`]:
-    'border-btn-border text-text hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover',
+    'border-btn-border text-text-light hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover',
   [`${ButtonType.DEFAULT}_${ButtonVariant.SQUARE}`]:
-    'group justify-center bg-transparent border-transparent hover:text-text text-textWeak shadow-none hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover',
+    'group justify-center bg-transparent border-transparent hover:text-text-light text-text-weak shadow-none hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover',
+  [`${ButtonType.PRIMARY}_${ButtonVariant.SQUARE}`]:
+    'justify-center bg-transparent border-transparent shadow-none hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover'
+}
+
+export const DEFINE_BUTTON_CLASS_NAME_LIGHT: DefineButtonColorType = {
+  [`${ButtonType.DEFAULT}_${ButtonVariant.GHOST}`]:
+    'group border-btn-border text-text-weak hover:bg-btn-bgHover hover:text-text-light active:bg-btn-bgActive active:border-btn-borderHover',
+  [`${ButtonType.PRIMARY}_${ButtonVariant.GHOST}`]:
+    'border-btn-border text-text-weak hover:text-text-dark hover:bg-btn-lightBgHover hover:border-btn-lightBorderHover active:border-btn-lightBorderActive active:bg-btn-lightBgActive',
+  [`${ButtonType.DEFAULT}_${ButtonVariant.SQUARE}`]:
+    'group justify-center bg-transparent border-transparent hover:text-text-light text-text-weak shadow-none hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover',
   [`${ButtonType.PRIMARY}_${ButtonVariant.SQUARE}`]:
     'justify-center bg-transparent border-transparent shadow-none hover:bg-btn-bgHover active:bg-btn-bgActive active:border-btn-borderHover'
 }
@@ -52,8 +64,9 @@ const Button: React.FC<ButtonCustomProps> = ({
   className,
   ...props
 }) => {
+  const [darkMode] = useTheme()
   const key: `${ButtonType}_${ButtonVariant}` = `${type}_${variant}`
-  const DEFAULT_CLASS_NAME = DEFINE_BUTTON_CLASS_NAME[key]
+  const DEFAULT_CLASS_NAME = darkMode ? DEFINE_BUTTON_CLASS_NAME_DARK[key] : DEFINE_BUTTON_CLASS_NAME_LIGHT[key]
   const DEFAULT_ICON_CLASS_NAME = DEFINE_BUTTON_ICON_CLASS_NAME[key] || ''
   const DEFAULT_PROPS: { [key: string]: boolean | string } = {}
 
